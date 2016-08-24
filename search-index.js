d6eec438d628af878ce6857d0c38b87e0cdb3531
Maciej Goszczycki <mgoszcz2@gmail.com>
2016-08-24 01:01:09 +0100

    First docs

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["serde_wat"] = {"doc":"# Scary macros for serde_json\n![Wat](https://i.imgur.com/IppKJ.jpg)","items":[[14,"wat","serde_wat","",null,null]],"paths":[]};
+initSearch(searchIndex);
